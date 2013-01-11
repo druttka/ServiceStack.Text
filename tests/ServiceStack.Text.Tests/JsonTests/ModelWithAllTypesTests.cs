011cd9b3e23384ad2b6605321aaffce263b10589
David Ruttka <druttka@3-gis.com>
2013-01-11 10:26:10 -0600

    Add passing tests around deserialization of ModelWithAllTypes

--- tests/ServiceStack.Text.Tests/JsonTests/ModelWithAllTypesTests.cs
+++ tests/ServiceStack.Text.Tests/JsonTests/ModelWithAllTypesTests.cs
@@ -43,6 +43,36 @@ namespace ServiceStack.Text.Tests.JsonTests
 			Console.WriteLine(s);
 		}
 
+        [Test]
+        public void Can_Deserialize()
+        {
+            var json = "{\"CharValue\":\"c\"}";
+            var model = JsonSerializer.DeserializeFromString<ModelWithAllTypes>(json);
+
+            Assert.AreEqual('c', model.CharValue);
+        }
+
+        [Test]
+        public void Can_Deerialize_list()
+        {
+            var json = "[{\"CharValue\":\"c\"},{\"CharValue\":\"d\"}]";
+            var model = JsonSerializer.DeserializeFromString<List<ModelWithAllTypes>>(json);
+
+            Assert.AreEqual(2, model.Count);
+            Assert.AreEqual('c', model[0].CharValue);
+            Assert.AreEqual('d', model[1].CharValue);
+        }
+
+        [Test]
+        public void Can_Deserialize_map()
+        {
+            var json = "{\"C\":{\"CharValue\":\"c\"},\"D\":{\"CharValue\":\"d\"}}";
+            var model = JsonSerializer.DeserializeFromString<Dictionary<string, ModelWithAllTypes>>(json);
+
+            Assert.AreEqual(2, model.Keys.Count);
+            Assert.AreEqual('c', ((ModelWithAllTypes)model["C"]).CharValue);
+            Assert.AreEqual('d', ((ModelWithAllTypes)model["D"]).CharValue);
+        }
 
 	}
 }
